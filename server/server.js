875696bfca138ea65149a061b4b9f4edde3a515c
Shubham Jain <shubhamj@aosmithindia.com>
2020-05-11 00:51:31 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.listen(3000, () => {
     console.log('Started the app on port 3000');
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 module.exports = {
     app
 };
