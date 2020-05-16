fb76956d75d75e369779ef9685c6c42fa574fb22
Shubham Jain <shubhamj@aosmithindia.com>
2020-05-16 19:15:01 +0530

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -21,9 +22,6 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started the app on port 3000');
-});
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
@@ -38,7 +36,7 @@ app.get('/todos/:id', (req, res) => {
     if(!ObjectID.isValid(id)){
         res.status(404).send({message: 'Invalid ID'});
     }
-
+    
     Todo.findOne(id).then((todos) => {
         if(!todos){
             res.status(404).send({message: 'No Results Found'});
@@ -47,6 +45,10 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send({e});
     });
+});
+
+app.listen(port, () => {
+    console.log(`Started the app on port ${port}`);
 });
 
 module.exports = {
