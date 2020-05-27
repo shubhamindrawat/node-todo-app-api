e158a88bd88ecb515b98956e8750a434e138e05e
Shubham Jain <shubhamj@aosmithindia.com>
2020-05-27 23:58:42 +0530

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -122,7 +122,15 @@ app.post('/users/login', (req, res) => {
     }).catch((e) => {
         res.sendStatus(400).send();
     });
-})
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
+});
 
 app.listen(port, () => {
     console.log(`Started the app on port ${port}`);
